e0145265d92eeb36b54ef5db3ec1473074affe4c
Devan Cakra Mudra Wijaya <54527592+cakraawijaya@users.noreply.github.com>
2025-10-14 07:10:22 +0700

    Update: home-page.vue

--- src/components/pages/home-page.vue
+++ src/components/pages/home-page.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="beranda" style="text-align: center">
         <img src="favicon/logo.png" class="Logo" alt="Logo"/>
-        <h1>Selamat datang di website perpusvue404</h1>
-        <p>- Website perpusvue404 ini merupakan media pustaka digital yang dibuat dengan bantuan framework Vue JS -</p>
+        <h1>Selamat datang di website Bookshelf-Vue</h1>
+        <p>- Website Bookshelf-Vue ini merupakan media pustaka digital yang dibuat dengan bantuan framework Vue JS -</p>
     </div>
 </template>
 
